7d3dcdce55a590dcab56f3302054247896aac224
Yanhao Li <hi@yanhaoli.com>
2020-12-12 22:24:07 -0500

    Part 1

--- src/Part1.java
+++ src/Part1.java
@@ -2,6 +2,23 @@ import java.util.*;
 class SortedList<T extends Comparable<T>> extends ArrayList<T> implements Comparable<SortedList<T>>{
     @Override
     public int compareTo(SortedList<T> sortedList) {
+        int i;
+        for (i = 0; i < this.size() && i < sortedList.size(); i++) {
+            if (this.get(i).compareTo(sortedList.get(i)) < 0) {
+                return -1;
+            } else if (this.get(i).compareTo(sortedList.get(i)) > 0) {
+                return 1;
+            } else {
+                continue;
+            }
+        }
+
+        if (i == this.size() && i < sortedList.size()) {
+            return -1;
+        } else if (i < this.size() && i == sortedList.size()) {
+            return 1;
+        }
+
         return 0;
     }
 
@@ -33,11 +50,80 @@ class SortedList<T extends Comparable<T>> extends ArrayList<T> implements Compar
 
 }
 
+class A implements Comparable<A> {
+    Integer value;
+
+    A(Integer x) {
+        this.value = x;
+    }
+
+    public int compareTo(A el) {
+        if (this.value < el.value) {
+            return -1;
+        } else if (this.value > el.value) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public String toString() {
+        return "A<" + this.value + ">";
+    }
+}
+
+class B extends A {
+    Integer x;
+    Integer y;
+    B(Integer x, Integer y) {
+        super(x + y);
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "B<" + this.x + "," + this.y + ">";
+    }
+}
+
 public class Part1 {
+    static <T extends Comparable<T>> void addToSortedList(SortedList<T> L, T z) {
+        L.add(z);
+    }
+
+    static void test() {
+        SortedList<A> c1 = new SortedList<A>();
+        SortedList<A> c2 = new SortedList<A>();
+        for(int i = 35; i >= 0; i-=5) {
+            addToSortedList(c1, new A(i));
+            addToSortedList(c2, new B(i+2,i+3));
+        }
+
+        System.out.print("c1: ");
+        System.out.println(c1);
+
+        System.out.print("c2: ");
+        System.out.println(c2);
+
+        switch (c1.compareTo(c2)) {
+            case -1:
+                System.out.println("c1 < c2");
+                break;
+            case 0:
+                System.out.println("c1 = c2");
+                break;
+            case 1:
+                System.out.println("c1 > c2");
+                break;
+            default:
+                System.out.println("Uh Oh");
+                break;
+        }
+
+    }
+
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<Integer>();
-        nums.add(2);
-        nums.add(1);
-        System.out.println(nums.toString());
+        test();
     }
 }
